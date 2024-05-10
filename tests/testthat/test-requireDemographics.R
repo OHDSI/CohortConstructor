063e8bb615b1d2d8aae508b95192433b07fe4e66
Núria Mercadé-Besora <61558739+nmercadeb@users.noreply.github.com>
2024-05-10 11:51:12 +0100

    demographics

--- tests/testthat/test-requireDemographics.R
+++ tests/testthat/test-requireDemographics.R
@@ -23,19 +23,53 @@ test_that("test it works and expected errors", {
   expect_true(all(cdm$cohort1 |> dplyr::pull("subject_id") == c(1, 1, 3, 4)))
   expect_true(all(cdm$cohort1 |> dplyr::pull("cohort_start_date") ==
                     c("2001-05-30", "2003-05-02", "2015-01-27", "1996-06-30")))
+  expect_equal(
+    settings(cdm$cohort1),
+    dplyr::tibble(
+      cohort_definition_id = 1L,
+      cohort_name = "cohort_1",
+      age_range = "0_35",
+      sex = "Both",
+      min_prior_observation = 10,
+      min_future_observation = 40
+    )
+  )
+  expect_true(all(
+    attrition(cdm$cohort1)$reason ==
+      c('Initial qualifying events', 'Age requirement: 0 to 35',
+        'Sex requirement: Both', 'Prior observation requirement: 10 days',
+        'Future observation requirement: 40 days')
+  ))
 
   cdm$cohort <- cdm$cohort %>%
-    requireAge(ageRange = list(c(0, 35))) %>%
-    requireSex(sex = "Both") %>%
-    requirePriorObservation(minPriorObservation = 10) %>%
-    requireFutureObservation(minFutureObservation = 40)
+    requireAge(ageRange = list(c(0, 35)), keep = FALSE) %>%
+    requireSex(sex = "Both", keep = FALSE) %>%
+    requirePriorObservation(minPriorObservation = 10, keep = FALSE) %>%
+    requireFutureObservation(minFutureObservation = 40, keep = FALSE)
 
   expect_true("cohort_table" %in% class(cdm$cohort))
   expect_equal(omopgenerics::attrition(cdm$cohort),
                omopgenerics::attrition(cdm$cohort1))
-  expect_true(all(cdm$cohort |> dplyr::pull("subject_id") == c(1, 1, 3, 4)))
-  expect_true(all(cdm$cohort |> dplyr::pull("cohort_start_date") ==
-                    c("2001-05-30", "2003-05-02", "2015-01-27", "1996-06-30")))
+  expect_true(all(cdm$cohort |> dplyr::pull("subject_id") |> sort() == c(1, 1, 3, 4)))
+  expect_true(all(cdm$cohort |> dplyr::pull("cohort_start_date") |> sort() ==
+                    c("1996-06-30", "2001-05-30", "2003-05-02", "2015-01-27")))
+  expect_equal(
+    settings(cdm$cohort),
+    dplyr::tibble(
+      cohort_definition_id = 1L,
+      cohort_name = "cohort_1",
+      min_future_observation = 40,
+      min_prior_observation = 10,
+      sex = "Both",
+      age_range = "0_35"
+    )
+  )
+  expect_true(all(
+    attrition(cdm$cohort)$reason ==
+      c('Initial qualifying events', 'Age requirement: 0 to 35',
+        'Sex requirement: Both', 'Prior observation requirement: 10 days',
+        'Future observation requirement: 40 days')
+  ))
 
   # expect errors
   expect_error(requireDemographics(cohort = "cohort"))
@@ -119,17 +153,23 @@ test_that("restrictions applied to single cohort", {
                                    schema = "main")
 
   cdm$cohort1 <- cdm$cohort %>%
-    requireDemographics(ageRange = list(c(0, 5)), name = "cohort1")
+    requireDemographics(ageRange = list(c(0, 5)), name = "cohort1", keep = FALSE)
   expect_true("2001-07-30" == cdm$cohort1 %>% dplyr::pull("cohort_start_date"))
   expect_true(all(
     c("Initial qualifying events", "Age requirement: 0 to 5", "Sex requirement: Both",
       "Prior observation requirement: 0 days", "Future observation requirement: 0 days") ==
-    omopgenerics::attrition(cdm$cohort1)$reason))
+      omopgenerics::attrition(cdm$cohort1)$reason))
   expect_true(all(c("cohort_definition_id", "subject_id", "cohort_start_date", "cohort_end_date")
                   == colnames(cdm$cohort1)))
+  expect_true(settings(cdm$cohort1)$cohort_definition_id == 1)
+  expect_true(settings(cdm$cohort1)$cohort_name == "cohort_1")
+  expect_true(settings(cdm$cohort1)$age_range == "0_5")
+  expect_true(settings(cdm$cohort1)$sex == "Both")
+  expect_true(settings(cdm$cohort1)$min_prior_observation == 0)
+  expect_true(settings(cdm$cohort1)$min_future_observation == 0)
 
   cdm$cohort2 <- cdm$cohort %>%
-    requireDemographics(sex = "Male", name = "cohort2")
+    requireDemographics(sex = "Male", name = "cohort2", keep = FALSE)
   expect_equal(dplyr::collect(cdm$cohort)$cohort_start_date,
                dplyr::collect(cdm$cohort2)$cohort_start_date)
   expect_true(all(
@@ -138,6 +178,12 @@ test_that("restrictions applied to single cohort", {
       omopgenerics::attrition(cdm$cohort2)$reason))
   expect_true(all(c("cohort_definition_id", "subject_id", "cohort_start_date", "cohort_end_date")
                   == colnames(cdm$cohort2)))
+  expect_true(settings(cdm$cohort2)$cohort_definition_id == 1)
+  expect_true(settings(cdm$cohort2)$cohort_name == "cohort_1")
+  expect_true(settings(cdm$cohort2)$age_range == "0_150")
+  expect_true(settings(cdm$cohort2)$sex == "Male")
+  expect_true(settings(cdm$cohort2)$min_prior_observation == 0)
+  expect_true(settings(cdm$cohort2)$min_future_observation == 0)
 
   CDMConnector::cdm_disconnect(cdm)
 })
@@ -158,13 +204,14 @@ test_that("ignore existing cohort extra variables", {
     dplyr::compute(name = "cohort", temporary = FALSE)
 
   cdm$cohort <- cdm$cohort |>
-    requirePriorObservation(minPriorObservation = 450)
+    requirePriorObservation(minPriorObservation = 450, keep = FALSE)
   expect_true(all(colnames(cdm$cohort) ==
-                c("cohort_definition_id", "subject_id", "cohort_start_date", "cohort_end_date",
-                  "age", "sex", "prior_observation", "future_observation")))
+                    c("cohort_definition_id", "subject_id", "cohort_start_date", "cohort_end_date",
+                      "age", "sex", "prior_observation", "future_observation")))
   expect_true(cdm$cohort |> dplyr::tally() |> dplyr::pull() == 2)
   expect_true(all(c("Initial qualifying events", "Prior observation requirement: 450 days") ==
                     omopgenerics::attrition(cdm$cohort)$reason))
+  expect_true(all(colnames(settings(cdm$cohort)) == c("cohort_definition_id", "cohort_name", "min_prior_observation")))
 
   cdm$new_cohort <- cdm$cohort |>
     requirePriorObservation(minPriorObservation = 450, name = "new_cohort")
@@ -175,6 +222,9 @@ test_that("ignore existing cohort extra variables", {
   expect_true(all(c("Initial qualifying events", "Prior observation requirement: 450 days",
                     "Prior observation requirement: 450 days") ==
                     omopgenerics::attrition(cdm$new_cohort)$reason))
+  expect_true(all(colnames(settings(cdm$cohort)) == c("cohort_definition_id", "cohort_name", "min_prior_observation")))
+
+  CDMConnector::cdm_disconnect(cdm)
 })
 
 test_that("external columns kept after requireDemographics", {
@@ -200,6 +250,8 @@ test_that("external columns kept after requireDemographics", {
     requireDemographics(indexDate = "new_index_date", ageRange = list(c(0,5)))
 
   expect_true(all(c("col_extra1", "col_extra2", "new_index_date") %in% colnames(cdm$cohort)))
+
+  CDMConnector::cdm_disconnect(cdm)
 })
 
 test_that("cohortIds", {
@@ -215,13 +267,217 @@ test_that("cohortIds", {
     cdm = cdm_local,
     schema = "main")
 
- cdm$new_cohort <- requireSex(cohort = cdm$cohort, cohortId = 1, sex = "Male") |>
+  cdm$new_cohort <- requireSex(cohort = cdm$cohort, cohortId = 1, sex = "Male") |>
     requirePriorObservation(cohortId = 3, minPriorObservation = 1000, name = "new_cohort")
- expect_true(all(
-   omopgenerics::attrition(cdm$new_cohort)$reason ==
-     c("Initial qualifying events", "Sex requirement: Male", "Initial qualifying events" ,
-       "Initial qualifying events", "Prior observation requirement: 1000 days")
- ))
- expect_true(all(cdm$new_cohort |> dplyr::pull("cohort_definition_id") == c(2,2,2,3)))
- expect_true(all(cdm$new_cohort |> dplyr::pull("subject_id") == c(2,2,2,1)))
+  expect_true(all(
+    omopgenerics::attrition(cdm$new_cohort)$reason ==
+      c("Initial qualifying events", "Sex requirement: Male", "Initial qualifying events" ,
+        "Initial qualifying events", "Prior observation requirement: 1000 days")
+  ))
+  expect_true(all(cdm$new_cohort |> dplyr::pull("cohort_definition_id") == c(2,2,2,3)))
+  expect_true(all(cdm$new_cohort |> dplyr::pull("subject_id") == c(2,2,2,1)))
+
+  CDMConnector::cdm_disconnect(cdm)
 })
+
+test_that("test more than one restriction", {
+  cdm_local <- omock::mockCdmReference() |>
+    omock::mockPerson(n = 3) |>
+    omock::mockObservationPeriod() |>
+    omock::mockCohort(numberCohorts = 3)
+  # to remove in new omock
+  cdm_local$person <- cdm_local$person |>
+    dplyr::mutate(dplyr::across(dplyr::ends_with("of_birth"), ~ as.numeric(.x)))
+  cdm <- CDMConnector::copy_cdm_to(
+    con = DBI::dbConnect(duckdb::duckdb(), ":memory:"),
+    cdm = cdm_local,
+    schema = "main")
+
+  # keep = false
+  cdm$cohort1 <- cdm$cohort |>
+    requireAge(ageRange = list(c(0,19), c(20, 40), c(0, 40)), name = "cohort1")
+  expect_true(all(
+    cdm$cohort1 |> dplyr::pull("cohort_definition_id") |> sort() ==
+      c(1, 1, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 8, 8, 8, 9, 9)
+  ))
+  expect_true(all(
+    cdm$cohort1 |> dplyr::pull("cohort_start_date") |> sort() ==
+      c('1999-08-27', '1999-08-27', '2000-01-07', '2000-01-07', '2000-04-10',
+        '2000-04-10', '2001-02-13', '2001-02-13', '2001-07-30', '2001-07-30',
+        '2003-03-02', '2003-03-02', '2004-01-21', '2004-01-21', '2015-01-25',
+        '2015-01-25', '2015-04-09', '2015-04-09')
+  ))
+  expect_true(all(
+    cdm$cohort1 |> dplyr::pull("cohort_end_date") |> sort() ==
+      c('2000-04-09', '2000-04-09', '2001-02-12', '2001-02-12', '2001-03-23',
+        '2001-03-23', '2001-07-15', '2001-07-15', '2004-01-20', '2004-01-20',
+        '2004-07-30', '2004-07-30', '2005-05-04', '2005-05-04', '2015-06-16',
+        '2015-06-16', '2015-06-26', '2015-06-26')
+  ))
+  expect_true(all(
+    attrition(cdm$cohort1)$reason |> sort() ==
+      c('Age requirement: 0 to 19', 'Age requirement: 0 to 19', 'Age requirement: 0 to 19',
+        'Age requirement: 0 to 40', 'Age requirement: 0 to 40', 'Age requirement: 0 to 40',
+        'Age requirement: 20 to 40', 'Age requirement: 20 to 40', 'Age requirement: 20 to 40',
+        'Initial qualifying events', 'Initial qualifying events', 'Initial qualifying events',
+        'Initial qualifying events', 'Initial qualifying events', 'Initial qualifying events',
+        'Initial qualifying events', 'Initial qualifying events', 'Initial qualifying events')
+  ))
+  expect_true(all(
+    attrition(cdm$cohort1)$number_records |> sort() ==
+      c(0, 1, 1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3)
+  ))
+  expect_true(all(settings(cdm$cohort1)$age_range |> unique() == c("0_19", "0_40", "20_40")))
+
+  # keep = true
+  cdm$cohort2 <- cdm$cohort |>
+    requireAge(ageRange = list(c(0,19), c(20, 40), c(0, 40)), name = "cohort2", keep = TRUE)
+  expect_true(all(
+    cdm$cohort2 |> dplyr::pull("cohort_definition_id") |> sort() ==
+      c(1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 11, 11, 11, 12, 12)
+  ))
+  expect_true(all(
+    cdm$cohort2 |> dplyr::pull("cohort_start_date") |> sort() ==
+      c('1999-08-27', '1999-08-27', '1999-08-27', '2000-01-07', '2000-01-07', '2000-01-07',
+        '2000-04-10', '2000-04-10', '2000-04-10', '2001-02-13', '2001-02-13', '2001-02-13',
+        '2001-07-30', '2001-07-30', '2001-07-30', '2003-03-02', '2003-03-02', '2003-03-02',
+        '2004-01-21', '2004-01-21', '2004-01-21', '2015-01-25', '2015-01-25', '2015-01-25',
+        '2015-04-09', '2015-04-09', '2015-04-09')
+  ))
+  expect_true(all(
+    cdm$cohort2 |> dplyr::pull("cohort_end_date") |> sort() ==
+      c('2000-04-09', '2000-04-09', '2000-04-09', '2001-02-12', '2001-02-12', '2001-02-12',
+        '2001-03-23', '2001-03-23', '2001-03-23', '2001-07-15', '2001-07-15', '2001-07-15',
+        '2004-01-20', '2004-01-20', '2004-01-20', '2004-07-30', '2004-07-30', '2004-07-30',
+        '2005-05-04', '2005-05-04', '2005-05-04', '2015-06-16', '2015-06-16', '2015-06-16',
+        '2015-06-26', '2015-06-26', '2015-06-26')
+  ))
+  expect_true(all(
+    attrition(cdm$cohort2)$reason |> sort() ==
+      c('Age requirement: 0 to 19', 'Age requirement: 0 to 19', 'Age requirement: 0 to 19',
+        'Age requirement: 0 to 40', 'Age requirement: 0 to 40', 'Age requirement: 0 to 40',
+        'Age requirement: 20 to 40', 'Age requirement: 20 to 40', 'Age requirement: 20 to 40',
+        'Initial qualifying events', 'Initial qualifying events', 'Initial qualifying events',
+        'Initial qualifying events', 'Initial qualifying events', 'Initial qualifying events',
+        'Initial qualifying events', 'Initial qualifying events', 'Initial qualifying events',
+        'Initial qualifying events', 'Initial qualifying events', 'Initial qualifying events')
+  ))
+  expect_true(all(
+    attrition(cdm$cohort2)$number_records |> sort() ==
+      c(0, 1, 1, 2, 2, rep(3, 16))
+  ))
+  expect_true(all(settings(cdm$cohort2)$age_range |> unique() == c("0_150", "0_19", "0_40", "20_40")))
+  expect_true(all(colnames(settings(cdm$cohort2)) %in% c("cohort_definition_id", "cohort_name", "age_range")))
+  expect_true(all(
+    settings(cdm$cohort2)$cohort_name |> sort() ==
+      c('cohort_1', 'cohort_1_1', 'cohort_1_2', 'cohort_1_3', 'cohort_2', 'cohort_2_1',
+        'cohort_2_2', 'cohort_2_3', 'cohort_3', 'cohort_3_1', 'cohort_3_2', 'cohort_3_3')
+  ))
+  expect_true(all(
+    settings(cdm$cohort2) |> dplyr::filter(.data$cohort_definition_id %in% 1:3) |> dplyr::pull(cohort_name) ==
+      c("cohort_1", "cohort_2", "cohort_3")))
+
+  # keep = true + no name
+  cdm$cohort <- cdm$cohort |>
+    requireAge(ageRange = list(c(0,19), c(20, 40), c(0, 40)), keep = TRUE)
+  expect_true(all(
+    cdm$cohort |> dplyr::pull("cohort_definition_id") |> sort() ==
+      c(1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 11, 11, 11, 12, 12)
+  ))
+  expect_true(all(
+    attrition(cdm$cohort)$reason |> sort() ==
+      c('Age requirement: 0 to 19', 'Age requirement: 0 to 19', 'Age requirement: 0 to 19',
+        'Age requirement: 0 to 40', 'Age requirement: 0 to 40', 'Age requirement: 0 to 40',
+        'Age requirement: 20 to 40', 'Age requirement: 20 to 40', 'Age requirement: 20 to 40',
+        'Initial qualifying events', 'Initial qualifying events', 'Initial qualifying events',
+        'Initial qualifying events', 'Initial qualifying events', 'Initial qualifying events',
+        'Initial qualifying events', 'Initial qualifying events', 'Initial qualifying events',
+        'Initial qualifying events', 'Initial qualifying events', 'Initial qualifying events')
+  ))
+  expect_true(all(
+    attrition(cdm$cohort)$number_records |> sort() ==
+      c(0, 1, 1, 2, 2, rep(3, 16))
+  ))
+  expect_true(all(settings(cdm$cohort2)$age_range |> unique() == c("0_150", "0_19", "0_40", "20_40")))
+  expect_true(all(colnames(settings(cdm$cohort2)) %in% c("cohort_definition_id", "cohort_name", "age_range")))
+  expect_true(all(
+    settings(cdm$cohort)$cohort_name |> sort() ==
+      c('cohort_1', 'cohort_1_1', 'cohort_1_2', 'cohort_1_3', 'cohort_2', 'cohort_2_1',
+        'cohort_2_2', 'cohort_2_3', 'cohort_3', 'cohort_3_1', 'cohort_3_2', 'cohort_3_3')
+  ))
+  expect_true(all(
+    settings(cdm$cohort) |> dplyr::filter(.data$cohort_definition_id %in% 1:3) |> dplyr::pull(cohort_name) ==
+      c("cohort_1", "cohort_2", "cohort_3")))
+
+  CDMConnector::cdm_disconnect(cdm)
+
+  # one empty output cohort
+  cdm_local <- omock::mockCdmReference() |>
+    omock::mockPerson(n = 3) |>
+    omock::mockObservationPeriod() |>
+    omock::mockCohort(numberCohorts = 3, seed = 4)
+  # to remove in new omock
+  cdm_local$person <- cdm_local$person |>
+    dplyr::mutate(dplyr::across(dplyr::ends_with("of_birth"), ~ as.numeric(.x)))
+  cdm <- CDMConnector::copy_cdm_to(
+    con = DBI::dbConnect(duckdb::duckdb(), ":memory:"),
+    cdm = cdm_local,
+    schema = "main")
+
+  # empty cohort return (no males in the cohort)
+  cdm$cohort1 <- cdm$cohort |>
+    requireSex(sex = c("Both", "Male"), name = "cohort1") |>
+    requirePriorObservation(minPriorObservation = 3, name = "cohort1")
+
+  expect_true(all(
+    cdm$cohort1 |> dplyr::pull("cohort_definition_id") |> sort() ==
+      cdm$cohort |> dplyr::pull("cohort_definition_id") |> sort()
+  ))
+  expect_true(all(
+    cdm$cohort1 |> dplyr::pull("cohort_start_date") |> sort() ==
+      cdm$cohort |> dplyr::pull("cohort_start_date") |> sort()
+  ))
+  expect_true(all(
+    cdm$cohort1 |> dplyr::pull("cohort_end_date") |> sort() ==
+      cdm$cohort |> dplyr::pull("cohort_end_date") |> sort()
+  ))
+  expect_true(all(
+    attrition(cdm$cohort1)$reason |> sort() ==
+      c('Initial qualifying events', 'Initial qualifying events', 'Initial qualifying events',
+        'Initial qualifying events', 'Initial qualifying events', 'Initial qualifying events',
+        'Prior observation requirement: 3 days', 'Prior observation requirement: 3 days',
+        'Prior observation requirement: 3 days', 'Prior observation requirement: 3 days',
+        'Prior observation requirement: 3 days', 'Prior observation requirement: 3 days',
+        'Sex requirement: Both', 'Sex requirement: Both', 'Sex requirement: Both',
+        'Sex requirement: Male', 'Sex requirement: Male', 'Sex requirement: Male')
+  ))
+  expect_true(all(
+    attrition(cdm$cohort1)$number_records |> sort() ==
+      c(rep(0, 6), rep(3, 12))
+  ))
+  expect_true(all(settings(cdm$cohort1)$min_prior_observation |> unique() == 3))
+  expect_true(all(colnames(settings(cdm$cohort1)) %in% c("cohort_definition_id", "cohort_name", "min_prior_observation", "sex")))
+  expect_true(all(
+    settings(cdm$cohort1)$cohort_name |> sort() ==
+      c("cohort_1", "cohort_1_2", "cohort_2", "cohort_2_2", "cohort_3", "cohort_3_2")
+  ))
+
+  cdm$cohort2 <- cdm$cohort |>
+    requireSex(sex = c("Both", "Male"), name = "cohort2", keep = TRUE) |>
+    requirePriorObservation(minPriorObservation = c(3, 2), name = "cohort2")
+
+  CDMConnector::cdm_disconnect(cdm)
+})
+
+test_that("more than 1 requirement, cohort Id and keep", {
+
+
+  # add cohort Id
+
+
+})
+
+# TEST: settings with more columns
+# TEST keep
+# test keep with cohort ID
+# test codelist
