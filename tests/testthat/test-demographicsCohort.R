1db4ad517a33f16aafaf78c2c4a6786c7ae2318e
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2024-09-03 16:02:14 +0100

    first example

--- tests/testthat/test-demographicsCohort.R
+++ tests/testthat/test-demographicsCohort.R
@@ -30,5 +30,46 @@ test_that("input validation", {
       demographicsCohort(name = "cohort3", minPriorObservation = -15)
   )
 
+  expect_error(
+    cohort <- cdm |>
+      demographicsCohort(name = "cohort3", minPriorObservation = "15")
+  )
+
+  PatientProfiles::mockDisconnect(cdm)
+})
+
+test_that("Example: sex", {
+  skip_on_cran()
+  cdm_local <- omock::mockCdmReference() |>
+    omock::mockPerson(n = 4) |>
+    omock::mockObservationPeriod() |>
+    omock::mockCohort(name = c("cohort1"), numberCohorts = 5, seed = 2)
+  cdm <- cdm_local |> copyCdm()
+  expect_no_error(
+    cdm$cohort3 <- cdm |>
+      demographicsCohort(name = "cohort3", sex = "Male")
+  )
+
+  cdm$cohort3 <- cdm$cohort3 |>
+    PatientProfiles::addSex()
+
+  expect_true(all(cdm$cohort3 |> dplyr::pull("sex")== "Male"))
+
+  expect_true(
+    setequal((cdm$cohort3 |>
+                dplyr::pull("subject_id")),
+             (cdm$person |> dplyr::filter(gender_concept_id==8507) |> dplyr::pull("person_id"))
+    )
+  )
+
+  cdm$cohort3 <- cdm$cohort3 |>
+    dplyr::left_join(cdm$observation_period, by = c("subject_id" = "person_id")) |>
+    dplyr::mutate(check1 = (cohort_start_date == observation_period_start_date),
+                  check2 = (cohort_end_date == observation_period_end_date)) |>
+    dplyr::compute()
+
+  expect_true(all(cdm$cohort3 |> dplyr::pull("check1")== T))
+  expect_true(all(cdm$cohort3 |> dplyr::pull("check2")== T))
+
   PatientProfiles::mockDisconnect(cdm)
 })
