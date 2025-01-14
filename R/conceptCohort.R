9e55edd7e0afc6c405c70dd049a5e89d9bd2bce5
Núria Mercadé-Besora <61558739+nmercadeb@users.noreply.github.com>
2025-01-14 10:39:59 +0000

    document

--- R/conceptCohort.R
+++ R/conceptCohort.R
@@ -4,7 +4,8 @@
 #' `conceptCohort()` creates a cohort table from patient records
 #' from the clinical tables in the OMOP CDM.
 #'
-#' The following tables are currently supported for creating concept#' cohorts:
+#' The following tables are currently supported for creating concept
+#' cohorts:
 #' * condition_occurrence
 #' * device_exposure
 #' * drug_exposure
